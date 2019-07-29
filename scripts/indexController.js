cda668a9402a37c2ccfc6332320225a84975964e
Alexander Proschenko <alexander.proschenko@ontarget-group.com>
2019-07-29 18:42:12 +0300

    getting devices

--- scripts/indexController.js
+++ scripts/indexController.js
@@ -1,7 +1,8 @@
 
 var app = angular.module("dashboardApp", []);
 
-app.controller("indexController", function ($scope) {
-    $scope.firstName = "John";
-    $scope.lastName = "Doe";
+app.controller("indexController", function ($scope, $http) {
+    $http.get("http://92.100.36.182:8084/json.htm?type=devices").then(function (response) {
+        $scope.devices = response.data.result;
+    });
 });
